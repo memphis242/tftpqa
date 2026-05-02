cb6992ac3ec9f2b63b8985951d5cd0a5d8d95e5d
Abdullah Almosalami <abdullah.almosalami.eng@gmail.com>
2026-05-02 11:26:15 -0500

    cleanup: rmv unused tftp_err content

--- src/tftp_err.h
+++ src/tftp_err.h
@@ -1,10 +1,10 @@
 /**
  * @file tftp_err.h
- * @brief Internal application error codes and string table.
+ * @brief Internal application error codes.
  * @date Apr 10, 2026
  * @author Abdulla Almosalmi, @memphis242
  *
- * These are *application-level* errors (socket failures, config problems, etc.),
+ * These are *application-level* errors (syscall failures, config problems, etc.),
  * not TFTP protocol error codes (those live in tftp_pkt.h).
  */
 
@@ -29,12 +29,5 @@ enum TFTP_Err
 
    TFTP_ERR_COUNT,
 };
-
-/**
- * @brief Return a human-readable string for an application error code.
- * @param[in] err  The error code.
- * @return A static string describing the error. Never returns NULL.
- */
-const char *tftp_err_str(enum TFTP_Err err);
 
 #endif // TFTP_ERR_H
